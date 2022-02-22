74dd8c3a92f2f85e417cd2ddb38e6dc81dd8117f
Nick Visalli <nvisal1225@gmail.com>
2022-02-21 20:05:54 -0500

    :white_check_mark: test(fmt): improve decode fmt test coverage
    add more tests to decode fmt chunk

--- pkg/decoder/fmt_test.go
+++ pkg/decoder/fmt_test.go
@@ -2,10 +2,11 @@ package decoder
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
-func TestReadFMTChunk(t *testing.T) {
+func TestReadFMTChunk_Success(t *testing.T) {
 	b := []byte{
 		0x01, 0x00, // AudioFormat
 		0x02, 0x00, // NumChannels
@@ -44,5 +45,283 @@ func TestReadFMTChunk(t *testing.T) {
 
 	if f.BitsPerSample != 24 {
 		t.Errorf("Error: did not find the correct bits per sample. Found %d", f.BitsPerSample)
+	}
+}
+
+func TestReadFMTChunk_Fail_No_AudioFormat(t *testing.T) {
+	b := []byte{}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+
+}
+
+func TestReadFMTChunk_Fail_Short_AudioFormat(t *testing.T) {
+	b := []byte{0x01}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_No_NumChannels(t *testing.T) {
+	b := []byte{0x01, 0x00}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_Short_NumChannels(t *testing.T) {
+	b := []byte{0x01, 0x00, // AudioFormat
+		0x02}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_No_SampleRate(t *testing.T) {
+	b := []byte{0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_Short_SampleRate(t *testing.T) {
+	b := []byte{
+		0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44,
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_No_ByteRate(t *testing.T) {
+	b := []byte{0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_Short_ByteRate(t *testing.T) {
+	b := []byte{
+		0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+		0x98,
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_No_BlockAlign(t *testing.T) {
+	b := []byte{0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+		0x98, 0x09, 0x04, 0x00, // ByteRate
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_Short_BlockAlign(t *testing.T) {
+	b := []byte{
+		0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+		0x98, 0x09, 0x04, 0x00, // ByteRate
+		0x06,
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_No_BitsPerSample(t *testing.T) {
+	b := []byte{0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+		0x98, 0x09, 0x04, 0x00, // ByteRate
+		0x06, 0x00,
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("returned error is not \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadFMTChunk_Fail_Short_BitsPerSample(t *testing.T) {
+	b := []byte{
+		0x01, 0x00, // AudioFormat
+		0x02, 0x00, // NumChannels
+		0x44, 0xac, 0x00, 0x00, // SampleRate
+		0x98, 0x09, 0x04, 0x00, // ByteRate
+		0x06, 0x00, // BlockAlign
+		0x18,
+	}
+
+	r := bytes.NewReader(b)
+
+	f, err := readFMTChunk(r)
+
+	if f != nil {
+		t.Error("returned fmt chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("returned error is not \"Unexpected EOF\". received \"%s\"", err.Error())
 	}
 }
