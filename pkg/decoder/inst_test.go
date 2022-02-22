2543063b2234195bbad9b8223cee71c9e56b959d
Nick Visalli <nvisal1225@gmail.com>
2022-02-21 21:33:47 -0500

    :white_check_mark: test(inst): improve decode inst test coverage
    add tests for decode inst chunk

--- pkg/decoder/inst_test.go
+++ pkg/decoder/inst_test.go
@@ -2,10 +2,11 @@ package decoder
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
-func TestReadInstChunk(t *testing.T) {
+func TestReadInstChunk_Success(t *testing.T) {
 	b := []byte{0x3c, 0x00, 0x00, 0x00, 0x7f, 0x01, 0x7f, 0x00}
 	r := bytes.NewReader(b)
 
@@ -42,4 +43,137 @@ func TestReadInstChunk(t *testing.T) {
 		t.Errorf("Error: did not find the correct high velocity. Found %d", i.HighVelocity)
 	}
 
+}
+
+func TestReadInstChunk_Fail_No_UnshiftedNote(t *testing.T) {
+	b := []byte{}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_FineTuneDB(t *testing.T) {
+	b := []byte{0x3c}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_Gain(t *testing.T) {
+	b := []byte{0x3c, 0x00}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_LowNote(t *testing.T) {
+	b := []byte{0x3c, 0x00, 0x00}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_HighNote(t *testing.T) {
+	b := []byte{0x3c, 0x00, 0x00, 0x00}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_LowVelocity(t *testing.T) {
+	b := []byte{0x3c, 0x00, 0x00, 0x00, 0x7f}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadInstChunk_Fail_No_HighVelocity(t *testing.T) {
+	b := []byte{0x3c, 0x00, 0x00, 0x00, 0x7f, 0x01}
+	r := bytes.NewReader(b)
+
+	i, err := readInstChunk(r)
+
+	if i != nil {
+		t.Error("returned inst chunk is not nil")
+	}
+
+	if err == nil {
+		t.Error("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
 }
