4ea55fe38240b9b13c24d865ca0dc40b6b8978b1
Nick Visalli <nvisal1225@gmail.com>
2022-02-25 16:46:51 -0500

    :white_check_mark: test(note): improve decode note chunk coverage
    improve test coverage for decode note chunk

--- pkg/decoder/note.go
+++ pkg/decoder/note.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"bytes"
 	"encoding/binary"
-	"io"
 )
 
 type note struct {
@@ -13,29 +12,19 @@ type note struct {
 
 func readNoteChunk(r *bytes.Reader, size uint32) (*note, error) {
 	n := &note{}
-	bytesRead := 0
 	B32 := make([]byte, 4)
 
 	if err := binary.Read(r, binary.BigEndian, &B32); err != nil {
 		return nil, err
 	}
-	bytesRead += 4
 	n.CuePointID = removeNullCharacters(string(B32[:]))
 
-	for {
-		data := make([]byte, 1)
-		if err := binary.Read(r, binary.BigEndian, &data); err != nil {
-			if err == io.EOF || err == io.ErrUnexpectedEOF {
-				break
-			}
-			return nil, err
-		}
-		n.Data = n.Data + removeNullCharacters(string(data[:]))
-		bytesRead++
-		if uint32(bytesRead) == size {
-			break
-		}
+	// subtract 4 from the total size to account for the cue point ID
+	b := make([]byte, size-4)
+	if err := binary.Read(r, binary.BigEndian, &b); err != nil {
+		return nil, err
 	}
+	n.Data = removeNullCharacters(string(b[:]))
 
 	return n, nil
 }
--- pkg/decoder/note_test.go
+++ pkg/decoder/note_test.go
@@ -2,10 +2,11 @@ package decoder
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
-func TestReadNoteChunk(t *testing.T) {
+func TestReadNoteChunk_Success(t *testing.T) {
 	b := []byte{0x01, 0x00, 0x00, 0x00, 0x53, 0x61, 0x6d,
 		0x70, 0x6c, 0x65, 0x72, 0x20, 0x6c, 0x6f, 0x6f,
 		0x70, 0x00, 0x00}
@@ -19,5 +20,85 @@ func TestReadNoteChunk(t *testing.T) {
 
 	if n.Data != "Sampler loop" {
 		t.Errorf("Error: did not find the correct data. Found %s", n.Data)
+	}
+}
+
+func TestReadNoteChunk_Fail_No_Cue_Point_ID(t *testing.T) {
+	b := []byte{}
+
+	r := bytes.NewReader(b)
+
+	l, err := readNoteChunk(r, uint32(len(b)))
+
+	if l != nil {
+		t.Error("returned labl chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadNoteChunk_Fail_Short_Cue_Point_ID(t *testing.T) {
+	b := []byte{0x01, 0x00, 0x00}
+
+	r := bytes.NewReader(b)
+
+	l, err := readNoteChunk(r, uint32(len(b)))
+
+	if l != nil {
+		t.Error("returned labl chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("expected \"Unexpected EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadNoteChunk_Fail_No_Data(t *testing.T) {
+	b := []byte{0x01, 0x00, 0x00, 0x00}
+
+	r := bytes.NewReader(b)
+
+	l, err := readNoteChunk(r, uint32(12))
+
+	if l != nil {
+		t.Error("returned labl chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.EOF {
+		t.Errorf("expected \"EOF\". received \"%s\"", err.Error())
+	}
+}
+
+func TestReadNoteChunk_Fail_Short_Data(t *testing.T) {
+	b := []byte{0x01, 0x00, 0x00, 0x00, 0x00}
+
+	r := bytes.NewReader(b)
+
+	l, err := readNoteChunk(r, uint32(12))
+
+	if l != nil {
+		t.Error("returned labl chunk is not nil")
+	}
+
+	if err == nil {
+		t.Errorf("returned error is nil")
+	}
+
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("expected \"Unexpected EOF\". received \"%s\"", err.Error())
 	}
 }
